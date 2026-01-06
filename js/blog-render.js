f359b33c5ce5e4857c0fdc41c37861b25eaa3024
Edvinas Grotuzas <113600729+Abugelis@users.noreply.github.com>
2026-01-06 23:19:42 +0000

    Fix Blog-render script for relative paths to posts

--- js/blog-render.js
+++ js/blog-render.js
@@ -5,10 +5,10 @@ const grid = document.querySelector(".blog-content-grid");
 // Create cards for blog page , contains header/image/decoration/link and excerpt
 grid.innerHTML = blogPosts.map(post => `
   <article class="blog-post-card">
-    <a href="/blog/${post.slug}.html"><img src="${post.image}" alt="${post.country}"></a>
+    <a href="../blog/${post.slug}.html"><img src="${post.image}" alt="${post.country}"></a>
     <h3>${post.title}</h3>
     <div class="blog-post-decorative-line"></div>
     <p>${post.excerpt}</p>
-    <a href="/blog/${post.slug}.html" class="blog-btn">READ MORE &#11166;</a>
+    <a href="../blog/${post.slug}.html" class="blog-btn">READ MORE &#11166;</a>
   </article>
 `).join("");
